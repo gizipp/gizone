75a4d39be4e81159ca0bfdde0d7812f5b7d3f7ad
Gilang Aprianto <gizipp@softwareseni.com>
2015-03-11 11:48:38 +0700

    add elastic search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  Article.import
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     resources :links
   end
 
-  resources :articles
+  resources :articles do
+    collection { get :search }
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
