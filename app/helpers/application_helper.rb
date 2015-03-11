466626b7553ff990ab6430a13819888ee57112de
Gilang Aprianto <gizipp@softwareseni.com>
2015-03-11 10:50:59 +0700

    tags html cleaner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def get_content_from(uri, selector)
+    doc = Nokogiri::HTML(open(uri))
+    doc.css('script, link').each { |node| node.remove }
+    return doc.css(selector).text.split.join(" ")
+    end
 end
