f91771ab9c7b79849fdcbddc2143c98b0aeed560
Gilang Aprianto <gizipp@softwareseni.com>
2015-03-11 11:22:18 +0700

    fetch url with strip contect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def get_content_from(uri, selector)
+  def get_content_from(uri, selector='body')
     doc = Nokogiri::HTML(open(uri))
     doc.css('script, link').each { |node| node.remove }
     return doc.css(selector).text.split.join(" ")
-    end
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -4,16 +4,23 @@ class Link < ActiveRecord::Base
 
   def self.fetch_article
     self.white.each do |link|
-      @article = MetaInspector.new('http://'+link.blog.domain.to_s+link.to_s,
+      @article = MetaInspector.new('http://'+link.blog.domain.to_s+link.path.to_s,
               :warn_level => :store,
               :connection_timeout => 5, :read_timeout => 5)
       article = Article.new
       article.title = @article.best_title
       article.desc =  @article.description
-      article.content =  @article.description
+      article.content = Link.get_content_from('http://'+link.blog.domain.to_s+link.path.to_s)
       article.img = @article.images.best
-      article.url = 'http://'+link.blog.domain.to_s+link.to_s
+      article.url = 'http://'+link.blog.domain.to_s+link.path.to_s
       article.save!
     end
   end
+
+  private
+    def self.get_content_from(uri, selector='body')
+      doc = Nokogiri::HTML(open(uri))
+      doc.css('script, link').each { |node| node.remove }
+      return doc.css(selector).text.split.join(" ")
+    end
 end
