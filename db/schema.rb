dcaa57ef1136b1d399b71d2929b59c6e7f9b955e
Gilang Aprianto <gizipp@softwareseni.com>
2015-03-13 14:24:55 +0700

    add link ref

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225140721) do
+ActiveRecord::Schema.define(version: 20150313002441) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20150225140721) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "link_id"
   end
 
+  add_index "articles", ["link_id"], name: "index_articles_on_link_id"
   add_index "articles", ["url"], name: "index_articles_on_url", unique: true
 
   create_table "blogs", force: :cascade do |t|
