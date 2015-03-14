77ad0d561f104426d823249dac70c0d53af2cc3e
Gilang Aprianto <gizipp@softwareseni.com>
2015-03-14 09:43:24 +0700

    change initializer into appCtrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def index
+    Article.__elasticsearch__.create_index! force: true
+    Article.__elasticsearch__.refresh_index!
+
+    Article.import
+  end
 end
--- config/initializers/reindex_article.rb
+++
@@ -1,4 +0,0 @@
-# Article.__elasticsearch__.create_index! force: true
-# Article.__elasticsearch__.refresh_index!
-
-# Article.import
