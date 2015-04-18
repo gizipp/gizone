f3af287363908455f72094fef38cb783019486a4
Gilang Aprianto <gizipp@softwareseni.com>
2015-04-18 22:00:52 +0700

    refactoring modularity and algorithm

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -14,53 +14,66 @@ class Blog
 
   slug :domain
 
-  def links_count
-    Link.where(blog_id: self.id).count
+  def self.without_links
+    @blogs_zero_links = []
+    self.all.each do |blog|
+      @blogs_zero_links << blog if blog.links_count == 0
+    end
+    return @blogs_zero_links
   end
 
   def self.fetch_links
-    paths = []
-    self.fresh.each do |blog|
-      @homepage = MetaInspector.new(blog.domain,
-              :warn_level => :store,
-              :connection_timeout => 5, :read_timeout => 5,
-              :headers => { 'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" })
-      @homepage.links.internal.each do |link|
-        uri = URI(link)
-        paths << uri.path
-      end
-
-      # save
-      paths.uniq.each do |path|
-        Link.where(path: path).first_or_create(blog_id: blog.id)
-      end
-
-      self.add_one_num_of_crawled(blog.id, blog.num_of_crawled) if paths.uniq.count != 0
-
-      # blog.depth.to_i.times do
-      #   Link.where(blog_id: blog.id).each do |url|
-      #     paths_tmp = []
-      #     @single_page = MetaInspector.new(blog.domain+url.path,
-      #         :warn_level => :store,
-      #         :connection_timeout => 5, :read_timeout => 5,
-      #         :headers => { 'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" })
-      #     @single_page.links.internal.each do |link|
-      #       uri = URI(link)
-      #       paths_tmp << uri.path
-      #       paths = paths - paths_tmp
-      #     end
-      #   end
-      # end
-
-      # # save again
-      # paths.uniq.each do |path|
-      #   Link.where(path: path).first_or_create(blog_id: blog.id)
-      # end
+    self.shall_be_fetched.each do |blog|
+      paths = blog.fetch_paths(blog.inspect_webpage)
+      blog.save_path(paths)
+      blog.add_one_num_of_crawled if paths.uniq.count != 0
     end
   end
 
-  def self.add_one_num_of_crawled(blog_id, num_of_crawled)
-    blog = self.find(blog_id)
-    blog.update_attribute(:num_of_crawled, num_of_crawled + 1)
+  def self.shall_be_fetched
+    @blogs = []
+    self.fresh.each do |blog|
+      @blogs << blog
+    end
+    self.without_links.each do |blog|
+      @blogs << blog
+    end
+    return @blogs.uniq
+  end
+
+  def inspect_webpage
+    @webpage = MetaInspector.new(self.domain,
+      :warn_level => :store,
+      :connection_timeout => 5, :read_timeout => 5,
+      :headers => {
+        'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2)
+                         AppleWebKit/537.36 (KHTML, like Gecko)
+                         Chrome/40.0.2214.111
+                         Safari/537.36"
+      }
+    )
+  end
+
+  def fetch_paths(webpage)
+    paths = []
+    webpage.links.internal.each do |link|
+      uri = URI(link)
+      paths << uri.path
+    end
+    return paths
+  end
+
+  def save_path(paths)
+    paths.uniq.each do |path|
+      Link.where(path: path).first_or_create(blog_id: self.id)
+    end
+  end
+
+  def add_one_num_of_crawled
+    self.update_attribute(:num_of_crawled, self.num_of_crawled + 1)
+  end
+
+  def links_count
+    return Link.where(blog_id: self.id).count
   end
 end
